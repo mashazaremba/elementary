15dfba3c9e679d07ee1a58b80ef578bbc6d5fe25
Maria <zarembamasha@icloud.com>
2023-11-25 19:04:07 +0300

    2.5 тесты для Point

---
+++ src/test/java/ru/condition/PointTest.java
@@ -0,0 +1,53 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when12to34then2Dot82() {
+        double expected = 2.82;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 3;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when63to87then4Dot47() {
+        double expected = 4.47;
+        int x1 = 6;
+        int y1 = 3;
+        int x2 = 8;
+        int y2 = 7;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when16to48then3Dot6() {
+        double expected = 3.6;
+        int x1 = 1;
+        int y1 = 6;
+        int x2 = 4;
+        int y2 = 8;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+}
