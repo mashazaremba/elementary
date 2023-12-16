32e00dc5360adbb1f52479d5a46b31036701d097
Maria <zarembamasha@icloud.com>
2023-12-16 20:36:59 +0300

    5.2 names

--- src/main/java/ru/array/Definition.java
+++ src/main/java/ru/array/Definition.java
@@ -2,11 +2,14 @@ package ru.array;
 
 public class Definition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Fedor Fedorov";
+        names[2] = "Artem";
+        names[3] = "Maria";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
