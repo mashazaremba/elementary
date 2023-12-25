303560427fb88aca7e5b693411bce1731ff4ac25
Maria <zarembamasha@icloud.com>
2023-12-25 20:40:44 +0300

    5.14 array char

---
+++ src/main/java/ru/array/ArrayChar.java
@@ -0,0 +1,13 @@
+package ru.array;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != word[i]) {
+                return false;
+            }
+        }
+        return result;
+    }
+}
