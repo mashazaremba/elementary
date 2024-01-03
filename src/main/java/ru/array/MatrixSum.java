10682d7b0d717b7473702dcabe9bab066d764181
Maria <zarembamasha@icloud.com>
2024-01-03 20:48:27 +0300

    5.24 MatrixSum

---
+++ src/main/java/ru/array/MatrixSum.java
@@ -0,0 +1,28 @@
+package ru.array;
+
+public class MatrixSum {
+    public static int sum(int[][] array) {
+        int rsl = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                rsl += array[i][j];
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {
+                {4, 2, 3},
+                {7, 1},
+                {3, 5, 6, 4}
+        };
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                int val = array[row][cell];
+                System.out.println(val);
+            }
+        }
+    }
+
+}
