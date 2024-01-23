fcf9342ba3c281ca49afc3d2665580d335e3ae64
Maria <zarembamasha@icloud.com>
2024-01-23 21:24:56 +0300

    7.15 max

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -1,9 +1,16 @@
 package ru.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(max(first, second, third), fourth);
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -41,4 +41,25 @@ class MaxTest {
         int expected = 55;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax55To11To22Then55() {
+        int first = 55;
+        int second = 11;
+        int third = 22;
+        int result = Max.max(first, second, third);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax55To11To22To70Then70() {
+        int first = 55;
+        int second = 11;
+        int third = 22;
+        int fourth = 70;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 70;
+        assertThat(result).isEqualTo(expected);
+    }
 }
